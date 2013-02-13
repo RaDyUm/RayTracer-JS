dd44af2c6f2d4ce56d49614f14ac2c40084c0d88
Romain Reynaud <romr09@gmail.com>
2013-02-13 15:42:03 +0100

    plus de trucs

--- RT.js
+++ RT.js
@@ -1,5 +1,22 @@
-$.getJSON('Scene.json', function(Scene) {
-				//alert(Scene.Eye.X);
-				//Après on fait un boucle et puis c'est bon.
+$.getJSON('Scene.json', function (Scene) {
+				var ctx = startCanvas(Scene);
+				raytracer(Scene, ctx);
 			}
 )
+
+function raytracer (Scene, ctx)
+{
+	var i = 0;
+	//tu fais un boucle et puis c'est bon
+}
+
+function startCanvas (Scene)
+{
+	$("<canvas/>", {
+    	"id": "window"
+	}).appendTo("#container");
+	$('#window').attr("width", Scene.Window.Width+'px');
+	$('#window').attr("height", Scene.Window.Height+'px');
+	var ctx = document.getElementById('window').getContext("2d");
+	return ctx;
+}
